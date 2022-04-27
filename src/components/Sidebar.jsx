7cd9fb576465264165819ce72a5728637e52b865
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-27 20:11:38 +0530

    add sidebar list items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,13 @@ import {
   ListItemButton,
   ListItemText,
 } from "@mui/material";
-import { HomeSharp } from "@mui/icons-material";
+import {
+  HomeSharp,
+  ArticleSharp,
+  Settings,
+  AccountBox,
+  Storefront,
+} from "@mui/icons-material";
 
 import React from "react";
 
@@ -25,6 +31,46 @@ const Sidebar = () => {
               <HomeSharp />
             </ListItemIcon>
             <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#pages">
+            <ListItemIcon>
+              <ArticleSharp />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#pages">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#pages">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#settings">
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
           </ListItemButton>
         </ListItem>
       </List>
