b9b6247b382b2ef54e946d67794790c25ae58a62
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-27 20:02:26 +0530

    create sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,33 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import { HomeSharp } from "@mui/icons-material";
+
 import React from "react";
 
 const Sidebar = () => {
   return (
     <Box
       flex={1}
-      bgcolor="skyblue"
+      //   bgcolor="skyblue"
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <HomeSharp />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
