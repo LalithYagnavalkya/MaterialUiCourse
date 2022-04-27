193a7ba712e8f5f51ef7a23d3c738534e9f5ae43
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-27 19:50:21 +0530

    add menu in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import {
   AppBar,
+  Avatar,
   Badge,
   Box,
   InputBase,
+  Menu,
+  MenuItem,
   styled,
   Toolbar,
   Typography,
 } from "@mui/material";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
-import React from "react";
+import MailIcon from "@mui/icons-material/Mail";
+import React, { useState } from "react";
 import { display } from "@mui/system";
+import { Notifications } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -23,12 +28,22 @@ const Search = styled("div")(({ theme }) => ({
   width: "40%",
 }));
 const Icons = styled(Box)(({ theme }) => ({
-  backgroundColor: "white",
-  padding: "0 10px",
-  borderRadius: theme.shape.borderRadius,
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: { display: "flex" },
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
 }));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -40,11 +55,46 @@ const Navbar = () => {
           <InputBase placeholder="search" />
         </Search>
         <Icons>
-          <Badge badgeContent={4} color="primary">
-            <MailIcon color="action" />
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
           </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://lh3.googleusercontent.com/ogw/ADea4I4uaf1gUVTdOqrsotwd5lPCD1ojWe49X3mXvvSxRw=s32-c-mo"
+            onClick={(e) => setOpen(true)}
+          />
         </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://lh3.googleusercontent.com/ogw/ADea4I4uaf1gUVTdOqrsotwd5lPCD1ojWe49X3mXvvSxRw=s32-c-mo"
+            onClick={(e) => setOpen(true)}
+          />
+          <Typography variant="span">Lalith </Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        // anchorEl={}
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
